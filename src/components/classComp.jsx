63facdd756b68a387a9666fbd5be1af38bfd3bf3
Lukas <lukasdrinkwater97@gmail.com>
2023-09-11 20:13:13 +0100

    going through examples

---
+++ src/components/classComp.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+// In a class-based component, the state gets initialized
+// as a part of the constructor.
+
+class ClassInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+      inputVal: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState((state) => ({
+      ...state,
+      inputVal: e.target.value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((state) => ({
+      todos: state.todos.concat(state.inputVal),
+      inputVal: "",
+    }));
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>{this.props.name}</h3>
+        {/*The input field to enter the todos*/}
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task-entry">Enter a task: </label>
+          <input type="text" name="task-entry" value={this.state.inputVal} />
+          onChange={this.handleInputChange}
+          <button type="submit">Submit</button>
+        </form>
+        <h4>All the tasks!</h4>
+        {/* The list of all the To-Do's, displayed */}
+        <ul></ul>
+      </section>
+    );
+  }
+}
+
+export default ClassInput;
